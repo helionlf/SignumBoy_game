03382976c3cd75be448dbd56db8cfcec1598139e
Hélio Nogueira <heliofilhofhnlf@gmail.com>
2025-05-16 13:52:41 -0300

    fase de 1 á 5 completa
    falta colocar a letra p na fase 4

--- sgnumBoy/Scripts/global/global_miniGame_alfabeto.gd
+++ sgnumBoy/Scripts/global/global_miniGame_alfabeto.gd
@@ -3,7 +3,7 @@ extends Node
 
 var completed = false
 
-var current_phase = "fase_3"
+var current_phase = "fase_1"
 var current_question_index = 0
 
 var quiz_data = {
@@ -15,16 +15,28 @@ var quiz_data = {
 		{"question": "res://Assets/UI/cards/panda.png", "options": ["res://Assets/UI/alfabeto/A.png", "res://Assets/UI/alfabeto/B.png", "res://Assets/UI/alfabeto/C.png", "res://Assets/UI/alfabeto/D.png"], "correct": 3}
 	],
 	"fase_2": [
-		{"question": "res://Assets/UI/cards/panela.png", "options": ["res://Assets/UI/alfabeto/E.png", "res://Assets/UI/alfabeto/I.png", "res://Assets/UI/alfabeto/G.png", "res://Assets/UI/alfabeto/H.png"], "correct": 0},
-		{"question": "res://Assets/UI/cards/faca.png", "options": ["res://Assets/UI/alfabeto/E.png", "res://Assets/UI/alfabeto/I.png", "res://Assets/UI/alfabeto/G.png", "res://Assets/UI/alfabeto/H.png"], "correct": 1},
-		{"question": "res://Assets/UI/cards/seringa.png", "options": ["res://Assets/UI/alfabeto/E.png", "res://Assets/UI/alfabeto/I.png", "res://Assets/UI/alfabeto/G.png", "res://Assets/UI/alfabeto/H.png"], "correct": 2},
-		{"question": "res://Assets/UI/cards/chave.png", "options": ["res://Assets/UI/alfabeto/E.png", "res://Assets/UI/alfabeto/I.png", "res://Assets/UI/alfabeto/G.png", "res://Assets/UI/alfabeto/H.png"], "correct": 3}
+		{"question": "res://Assets/UI/cards/panela.png", "options": ["res://Assets/UI/alfabeto/E.png", "res://Assets/UI/alfabeto/F.png", "res://Assets/UI/alfabeto/G.png", "res://Assets/UI/alfabeto/H.png"], "correct": 0},
+		{"question": "res://Assets/UI/cards/faca.png", "options": ["res://Assets/UI/alfabeto/E.png", "res://Assets/UI/alfabeto/F.png", "res://Assets/UI/alfabeto/G.png", "res://Assets/UI/alfabeto/H.png"], "correct": 1},
+		{"question": "res://Assets/UI/cards/seringa.png", "options": ["res://Assets/UI/alfabeto/E.png", "res://Assets/UI/alfabeto/F.png", "res://Assets/UI/alfabeto/G.png", "res://Assets/UI/alfabeto/H.png"], "correct": 2},
+		{"question": "res://Assets/UI/cards/chave.png", "options": ["res://Assets/UI/alfabeto/E.png", "res://Assets/UI/alfabeto/F.png", "res://Assets/UI/alfabeto/G.png", "res://Assets/UI/alfabeto/H.png"], "correct": 3}
 	],
 	"fase_3": [
 		{"question": "res://Assets/UI/cards/limao.png", "options": ["res://Assets/UI/alfabeto/I.png", "res://Assets/UI/alfabeto/J.png", "res://Assets/UI/alfabeto/K.png", "res://Assets/UI/alfabeto/L.png"], "correct": 0},
 		{"question": "res://Assets/UI/cards/jaca.png", "options": ["res://Assets/UI/alfabeto/I.png", "res://Assets/UI/alfabeto/J.png", "res://Assets/UI/alfabeto/K.png", "res://Assets/UI/alfabeto/L.png"], "correct": 1},
 		{"question": "res://Assets/UI/cards/kiwi.png", "options": ["res://Assets/UI/alfabeto/I.png", "res://Assets/UI/alfabeto/J.png", "res://Assets/UI/alfabeto/K.png", "res://Assets/UI/alfabeto/L.png"], "correct": 2},
 		{"question": "res://Assets/UI/cards/laranja.png", "options": ["res://Assets/UI/alfabeto/I.png", "res://Assets/UI/alfabeto/J.png", "res://Assets/UI/alfabeto/K.png", "res://Assets/UI/alfabeto/L.png"], "correct": 3}
+	],
+	"fase_4": [
+		{"question": "res://Assets/UI/cards/musculo.png", "options": ["res://Assets/UI/alfabeto/M.png", "res://Assets/UI/alfabeto/N.png", "res://Assets/UI/alfabeto/O.png", "res://Assets/UI/alfabeto/L.png"], "correct": 0},
+		{"question": "res://Assets/UI/cards/nariz.png", "options": ["res://Assets/UI/alfabeto/M.png", "res://Assets/UI/alfabeto/N.png", "res://Assets/UI/alfabeto/O.png", "res://Assets/UI/alfabeto/L.png"], "correct": 1},
+		{"question": "res://Assets/UI/cards/coracao.png", "options": ["res://Assets/UI/alfabeto/M.png", "res://Assets/UI/alfabeto/N.png", "res://Assets/UI/alfabeto/O.png", "res://Assets/UI/alfabeto/L.png"], "correct": 2},
+		{"question": "res://Assets/UI/cards/pulmao.png", "options": ["res://Assets/UI/alfabeto/M.png", "res://Assets/UI/alfabeto/N.png", "res://Assets/UI/alfabeto/O.png", "res://Assets/UI/alfabeto/L.png"], "correct": 3}
+	],
+	"fase_5": [
+		{"question": "res://Assets/UI/cards/qatar.png", "options": ["res://Assets/UI/alfabeto/Q.png", "res://Assets/UI/alfabeto/R.png", "res://Assets/UI/alfabeto/S.png", "res://Assets/UI/alfabeto/T.png"], "correct": 0},
+		{"question": "res://Assets/UI/cards/brasil.png", "options": ["res://Assets/UI/alfabeto/Q.png", "res://Assets/UI/alfabeto/R.png", "res://Assets/UI/alfabeto/S.png", "res://Assets/UI/alfabeto/T.png"], "correct": 1},
+		{"question": "res://Assets/UI/cards/russia.png", "options": ["res://Assets/UI/alfabeto/Q.png", "res://Assets/UI/alfabeto/R.png", "res://Assets/UI/alfabeto/S.png", "res://Assets/UI/alfabeto/T.png"], "correct": 2},
+		{"question": "res://Assets/UI/cards/portugal.png", "options": ["res://Assets/UI/alfabeto/Q.png", "res://Assets/UI/alfabeto/R.png", "res://Assets/UI/alfabeto/S.png", "res://Assets/UI/alfabeto/T.png"], "correct": 3}
 	]
 }
 
@@ -35,14 +47,13 @@ func next_level():
 	elif current_phase == "fase_2":
 		current_phase = "fase_3"
 	elif current_phase == "fase_3":
+		current_phase = "fase_4"
+	elif current_phase == "fase_4":
+		current_phase = "fase_5"
+	elif current_phase == "fase_5":
 		current_phase = "completed"
 		completed = true
 	
-
-	#if current_phase == "fase_2":
-		#current_phase = "completed"
-	#TEM QUE CRIAR UM IF PRA CADA FASE
-		
 func exit():
 	completed = true
 	await get_tree().create_timer(5).timeout
