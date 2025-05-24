be47f546487e4e18b393be42d13f21e65128beb7
Hélio Nogueira <heliofilhofhnlf@gmail.com>
2025-05-24 12:51:22 -0300

    .

--- sgnumBoy/Scripts/lesson_board.gd
+++ sgnumBoy/Scripts/lesson_board.gd
@@ -3,9 +3,10 @@ extends Node2D
 
 func _ready():
 	$CanvasLayer/board/alfabeto.visible = false
-
+	$CanvasLayer/board/schoolMaterials.visible = false
 
 func _process(delta):
 	if GlobalMGA.completed:
 		$CanvasLayer/board/alfabeto.visible = true
-		
+	if GlobaSMM.completed:
+		$CanvasLayer/board/schoolMaterials.visible = true
