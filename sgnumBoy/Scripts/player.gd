c0399a7bd646193ae550e76efb37eb07005050e1
Hélio Nogueira <heliofilhofhnlf@gmail.com>
2025-05-26 13:34:25 -0300

    movimentação e animação do personagem

--- sgnumBoy/Scripts/player.gd
+++ sgnumBoy/Scripts/player.gd
@@ -1,15 +1,49 @@
 extends CharacterBody2D
 
 
-const SPEED = 80.0
+const SPEED = 30.0
+var direction = "south"
 
 func _physics_process(delta):
-	var direction: Vector2 = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
+	_move()
+	_update_animation()
+	move_and_slide()
+
+func _move():
+	var direction: Vector2 = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
 	if direction:
 		velocity = direction.normalized() * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+
+func _update_animation():
+	if velocity.length() > 0:
+		if velocity.x > 0:
+			$animation.play("walk")
+			$animation.flip_h = false
+			direction = "east"
+		elif velocity.x < 0:
+			$animation.play("walk")
+			$animation.flip_h = true
+			direction = "west"
+		elif velocity.y > 0:
+			$animation.play("down")
+			direction = "south"
+		elif velocity.y < 0:
+			$animation.play("up")
+			direction = "north"
+	else:
+		if direction == "east":
+			$animation.play("idle_ew")
+			$animation.flip_h = false
+		elif direction == "west":
+			$animation.play("idle_ew")
+			$animation.flip_h = true
+		elif direction == "south":
+			$animation.play("idle_s")
+		else:
+			$animation.play("idle_n")
 
 
-	move_and_slide()
