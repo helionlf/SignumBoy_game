9a3935f422fefc311a9028472a1ed917deab241a
Hélio Nogueira <heliofilhofhnlf@gmail.com>
2025-06-04 14:43:33 -0300

    board completo

--- sgnumBoy/Scripts/lesson_board.gd
+++ sgnumBoy/Scripts/lesson_board.gd
@@ -4,9 +4,20 @@ extends Node2D
 func _ready():
 	$CanvasLayer/board/alfabeto.visible = false
 	$CanvasLayer/board/schoolMaterials.visible = false
-
+	$CanvasLayer/board/schoolMaterials2.visible = false
+	$CanvasLayer/board/ingredientes.visible = false
+	$CanvasLayer/board/ingredientes2.visible = false
+	
 func _process(delta):
 	if GlobalMGA.completed:
 		$CanvasLayer/board/alfabeto.visible = true
 	if GlobalSMM.completed:
 		$CanvasLayer/board/schoolMaterials.visible = true
+		$CanvasLayer/board/schoolMaterials2.visible = true
+	if GlobalSM.completed:
+		$CanvasLayer/board/ingredientes.visible = true
+		$CanvasLayer/board/ingredientes2.visible = true
+
+func _input(event):
+	if event.is_action_pressed("sair"):
+		Transition.fade_to_scene("res://Scenes/tile_map_quarto.tscn")
