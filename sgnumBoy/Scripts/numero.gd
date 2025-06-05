138c1659ba5262b3dd87555ad3d094277c5f086d
Hélio Nogueira <heliofilhofhnlf@gmail.com>
2025-06-05 13:20:21 -0300

    minigame números adicionado

---
+++ sgnumBoy/Scripts/numero.gd
@@ -0,0 +1,33 @@
+extends Control
+
+@export var pos_inicial : Vector2
+@export var posicao_alvo : Vector2  # posição onde esse número deve ser solto
+@export var tolerancia := 40.0      # distância máxima para "encaixar"
+
+var dragging := false
+var offset := Vector2.ZERO
+
+func _ready():
+	position = pos_inicial
+	mouse_filter = Control.MOUSE_FILTER_PASS
+
+func _process(_delta):
+	if dragging:
+		global_position = get_global_mouse_position() - offset
+
+func _gui_input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			dragging = true
+			offset = get_global_mouse_position() - global_position
+		else:
+			dragging = false
+			# Verifica se está perto o suficiente da posição-alvo
+			if global_position.distance_to(posicao_alvo) <= tolerancia:
+				global_position = posicao_alvo  # Encaixa
+			else:
+				position = pos_inicial
+
+func _input(event):
+	if event.is_action_pressed("sair"):
+		Transition.fade_to_scene("res://Scenes/tile_map_scholl.tscn")
